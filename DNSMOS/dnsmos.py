67871e7ebf90372e09a75f81975aba83646c127d
vishakg <30301957+vishakg@users.noreply.github.com>
2020-11-10 16:42:20 -0800

    Update dnsmos.py
    Add CSV dump of scores

--- DNSMOS/dnsmos.py
+++ DNSMOS/dnsmos.py
@@ -2,9 +2,11 @@ import argparse
 import glob
 import json
 import os
+
+import numpy as np
+import pandas as pd
 import requests
 import soundfile as sf
-
 
 # URL for the web service
 SCORING_URI = '<Insert the url we provide in email here>'
@@ -18,6 +20,7 @@ headers = {'Content-Type': 'application/json'}
 headers['Authorization'] = f'Basic {AUTH_KEY }'
 def main(args):
     audio_clips_list = glob.glob(os.path.join(args.testset_dir, "*.wav"))
+    scores = []
     for fpath in audio_clips_list:
         audio, fs = sf.read(fpath)
         if fs != 16000:
@@ -26,7 +29,15 @@ def main(args):
         input_data = json.dumps(data)
         # Make the request and display the response
         resp = requests.post(SCORING_URI, data=input_data, headers=headers)
-        print(resp.json())
+        score_dict = resp.json()
+        score_dict['file_name'] = os.path.basename(fpath)
+        scores.append(score_dict)
+
+    df = pd.DataFrame(scores)
+    print('Mean MOS Score for the files is ', np.mean(df['mos']))
+
+    if args.score_file:
+        df.to_csv(args.score_file)
 
 
 
@@ -34,5 +45,7 @@ if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--testset_dir", required=True, 
                         help='Path to the dir containing audio clips to be evaluated')
+    parser.add_argument('--score_file', help='If you want the scores in a CSV file provide the full path')
     args = parser.parse_args()
     main(args)
+
