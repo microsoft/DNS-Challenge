6c33840dba05e01af6e3580020119b6bdbdf9b4f
Vishak Gopal <30301957+vishakg@users.noreply.github.com>
2021-10-28 15:01:15 -0700

    Add support for calling P.835 API
    Add support for calling P.835 API

--- DNSMOS/dnsmos.py
+++ DNSMOS/dnsmos.py
@@ -11,7 +11,8 @@ import soundfile as sf
 from urllib.parse import urlparse, urljoin
 
 # URL for the web service
-SCORING_URI = '<Insert the url we provide in email here>'
+SCORING_URI_DNSMOS = 'https://dnsmos.azurewebsites.net/score'
+SCORING_URI_DNSMOS_P835 = 'https://dnsmos.azurewebsites.net/v1/dnsmosp835/score'
 # If the service is authenticated, set the key or token
 AUTH_KEY = '<Insert the key we provide in email here>'
 
@@ -27,17 +28,23 @@ def main(args):
         audio, fs = sf.read(fpath)
         if fs != 16000:
             print('Only sampling rate of 16000 is supported as of now')
-        data = {"data": audio.tolist()}
+        data = {"data": audio.tolist(), "filename": os.path.basename(fpath)}
         input_data = json.dumps(data)
         # Make the request and display the response
-        u = urlparse(SCORING_URI)
-        resp = requests.post(urljoin("https://" + u.netloc, 'score'), data=input_data, headers=headers)
+        if args.method == 'p808':
+            u = SCORING_URI_DNSMOS
+        else:
+            u = SCORING_URI_DNSMOS_P835
+        resp = requests.post(u, data=input_data, headers=headers)
         score_dict = resp.json()
         score_dict['file_name'] = os.path.basename(fpath)
         scores.append(score_dict)
 
     df = pd.DataFrame(scores)
-    print('Mean MOS Score for the files is ', np.mean(df['mos']))
+    if args.method == 'p808':
+        print('Mean MOS Score for the files is ', np.mean(df['mos']))
+    else:
+        print('Mean scores for the files: SIG[{}], BAK[{}], OVR[{}]'.format(np.mean(df['mos_sig']),np.mean(df['mos_bak']),np.mean(df['mos_ovr'])))
 
     if args.score_file:
         df.to_csv(args.score_file)
@@ -49,6 +56,8 @@ if __name__=="__main__":
     parser.add_argument("--testset_dir", required=True, 
                         help='Path to the dir containing audio clips to be evaluated')
     parser.add_argument('--score_file', help='If you want the scores in a CSV file provide the full path')
+    parser.add_argument('--method', default='p808', const='p808', nargs='?', choices=['p808', 'p835'],
+                        help='Choose which method to compute P.808 or P.835. Default is P.808')
     args = parser.parse_args()
     main(args)
 
