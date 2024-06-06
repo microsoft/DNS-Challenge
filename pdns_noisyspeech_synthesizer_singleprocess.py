fb71b87410dcd5e606c0c9eff4d59ac4eac72571
Daniel Ohayon <danielohayon444@gmail.com>
2024-06-06 09:27:51 +0300

    Update pdns_noisyspeech_synthesizer_singleprocess.py

--- pdns_noisyspeech_synthesizer_singleprocess.py
+++ pdns_noisyspeech_synthesizer_singleprocess.py
@@ -570,18 +570,18 @@ def main_body():
 
     if cfg['rir_dir'] != 'None':
         rir_dir = cfg['rir_dir']
-        
-    print(clean_dir)
-    print(clean_dir2)
-    print(noise_dir)
-    print(spkid_csv)
-    print(rir_dir)
 
     if cfg['noise_dir'] != 'None':
         noise_dir = cfg['noise_dir']
     if not os.path.exists:
         assert False, ('Noise data is required')
 
+    print(clean_dir)
+    print(clean_dir2)
+    print(noise_dir)
+    print(spkid_csv)
+    print(rir_dir)
+    
     params['fs'] = int(cfg['sampling_rate'])
     params['audioformat'] = cfg['audioformat']
     params['audio_length'] = float(cfg['audio_length'])
